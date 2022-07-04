b6679778f9b38657241cdfe99acec55fb11fed8e
wenwd <1583925120@qq.com>
2022-07-04 22:10:48 +0800

    添加下载不需要登录的番剧的功能（720p以下非会员番剧）

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ module.exports = {
     is_av_or_Bv: downloadVideo.is_av_or_Bv,
     get_video_info: downloadVideo.get_video_info,
     get_download_url: downloadVideo.get_download_url,
-    download_video: downloadVideo.download_video,
+    download_video: downloadVideo.download_video, // 使用bv号或av号下载视频
     marge_stream: downloadVideo.marge_stream,
-    download_audio: downloadVideo.download_audio,
+    download_audio: downloadVideo.download_audio, // 使用bv号或av号下载音频
     get_clarity: downloadVideo.get_clarity,
-    get_bangumi_by_name: bangumi.get_bangumi_by_name,
+    get_bangumi_by_name: bangumi.get_bangumi_by_name, // 使用番剧名称下载番剧
     download_bangumi: bangumi.download_bangumi,
-    bangumiDefinition: bangumi.bangumiDefinition,
+    bangumiDefinition: bangumi.bangumiDefinition, // 番剧清晰度对象
 }
